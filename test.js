cd69f822e6b673d9e820fc17919ec687b57f864f
railscope <laudertoby53@gmail.com>
2026-09-14 15:48:36 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,1 @@
-var openrailwaymap = new ol.layer.Tile({
-    title: 'OpenRailwayMap',
-    visible: true,
-    source : new ol.source.XYZ({
-                attributions : [
-                    ol.source.OSM.ATTRIBUTION,
-                    new ol.Attribution({
-                        html : 'Style: <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA 2.0</a> <a href="https://www.openrailwaymap.org/">OpenRailwayMap</a> and <a href="https://osm.org/copyright"</a>OpenStreetMap</a>'
-                    })
-                ],
-                url : 'https://tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png',
-                crossOrigin: null, //make it work inside canvas
-                tilePixelRatio: 2, //server returns 512px img for 256 tiles
-                maxZoom: 19, // XYZ's default is 18
-                opaque: true
-            })
-        })
-map.addLayer(openrailwaymap);
+Data <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a>, Style: <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA 2.0</a> <a href="http://www.openrailwaymap.org/">OpenRailwayMap</a>
